fe3e6c1d46593e3abe8d76389dee687ad5e2bfbe
Phuc <pnguyen@macalester.edu>
2017-11-14 14:40:41 -0600

    R files to clean data

---
+++ clean_data.R
@@ -0,0 +1,21 @@
+#Crime = read.csv('Crimes_-_2001_to_present.csv')
+
+
+library(dplyr)
+library(lubridate)
+
+#Filter for year 2010 to present (2017) and clear some columns. Saved resulting data
+
+Crime = Crime %>%
+ mutate()
+filter(Year >= 2010) %>%
+  mutate(Arrest = as.numeric(Arrest == 'true'), Domestic = as.numeric(Domestic == 'true'), Date = as.POSIXct(strptime(Date, "%m/%d/%Y %H:%M:%S %p")))
+head(Crime)
+write.csv(Crime, file="Crime_2010_to_2017.csv")
+
+
+#Take a random sample to work with
+Crime = read.csv('Crime_2010_to_2017.csv')
+Crime_sample = Crime[sample(1:nrow(Crime), 10000,
+                             replace=FALSE),]
+write.csv(Crime_sample, file='Crime_sample_2010_to_2017.csv')
