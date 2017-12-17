83c523cea5ce6a2f621854b297e61bdf417c589e
Phuc <pnguyen@macalester.edu>
2017-12-17 10:52:21 -0600

    other changes

--- create_model_data.R
+++ create_model_data.R
@@ -102,6 +102,6 @@ Model_Data$racegr <- ifelse(Model_Data$percent_white >80, 1,
 Model_Data$income = scale(Model_Data$per_capita_income)[,1]
 
 # Save model data
-write.csv(Model_Data, "Crime_models_data.csv")
+#write.csv(Model_Data, "Crime_models_data.csv")
 #write.csv(Model_Data, "Crime_models_violent_data.csv")
 #write.csv(Model_Data, "Crime_models_nonviolent_data.csv")
